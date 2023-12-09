43724840c9dbd862ab30d87a1b59a62db3cfb943
MingyeomKim <mingyum119@naver.com>
2023-12-09 14:32:55 +0900

    feat: 코치가 못 먹는 메뉴의 구분자와 중복을 검증

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -22,25 +22,42 @@ public class InputView {
 
     private void validateBlankInput(String message) {
         if(message.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 빈 문자열이 입력되었습니다.");
+            throw new IllegalArgumentException(
+                    "[ERROR] 빈 문자열이 입력되었습니다."
+            );
         }
     }
 
-    private List<String> validateUnsupportedSeparators(String message) {
-        if (message.contains(",".repeat(2))
-                || message.startsWith(",") || message.endsWith(",")) {
-            throw new IllegalArgumentException("[ERROR] 올바르지 않은 구분자 입력입니다.");
-        }
-        return parseToList(message);
-    }
 
     private List<String> parseToList(String message) {
         return Arrays.stream(message.split(",")).toList();
     }
 
+    public List<Menu> enterForbiddenMenus(String name) {
+        System.out.println(name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+        List<String> names = validateForbiddenMenus(Console.readLine());
+        return List.of()
+    }
+
+    private List<String> validateForbiddenMenus(String message) {
+        List<String> names = validateUnsupportedSeparators(message);
+        validateDuplicated(names);
+
+    }
+
+    private List<String> validateUnsupportedSeparators(String message) {
+        if (message.contains(",".repeat(2))
+                || message.startsWith(",") || message.endsWith(",")) {
+            throw new IllegalArgumentException(
+                    "[ERROR] 올바르지 않은 구분자 입력입니다."
+            );
+        }
+        return parseToList(message);
+    }
+
     private void validateDuplicated(List<String> inputs) {
         if (isDuplicated(inputs)) {
-            throw new IllegalArgumentException("[ERROR] 중복된 입력입니다.")
+            throw new IllegalArgumentException("[ERROR] 중복된 입력입니다.");
         }
     }
 
@@ -50,11 +67,5 @@ public class InputView {
                 .toList();
 
         return names.size() != distinctNames.size();
-    }
-
-    public List<Menu> enterForbiddenMenus() {
-        System.out.println("토미(이)가 못 먹는 메뉴를 입력해 주세요.");
-
-
     }
 }
